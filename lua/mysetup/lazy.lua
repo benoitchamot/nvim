897fc5c64ff7af054a1a70df89421bae3a5c1433
Benoit Chamot <benoit.chamot@proton.me>
2025-11-14 09:32:52 +1100

    Delete lazydev which was always failing and clean things up in the LSP configs.

--- lua/mysetup/lazy.lua
+++ lua/mysetup/lazy.lua
@@ -21,21 +21,15 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  
-
-  -- Simple example
-
   -- Use this to import plugins specified in a plugins/
   spec = {
     -- import your plugins
-    { import = "plugins" }, 
+    { import = "plugins" },
   },
 
   -- Configure any other settings here. See the documentation for more details.
-  
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
